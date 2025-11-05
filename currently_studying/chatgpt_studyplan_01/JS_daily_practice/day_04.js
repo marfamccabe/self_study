f24946371546af04a48b73ef1ae734cf39216a1b
Marissa Wilson <marfawilsoncode@gmail.com>
2025-11-04 20:05:12 -0600

    Created initial files for day 4

---
+++ currently_studying/chatgpt_studyplan_01/JS_daily_practice/day_04.js
@@ -0,0 +1,11 @@
+// Challenge: The Playlist Organizer
+// Goal: Clean up & organize an array of song titles (remove duplicates, trim extra space, sort alphabetically, return cleaned-up playlist as a new array)
+
+const songs = [
+    "  Wonderwall  ",
+    "Bohemian Rhapsody",
+    "Hotel California",
+    "Bohemian Rhapsody",
+    "  Stairway to Heaven",
+    "Hotel California "
+];
